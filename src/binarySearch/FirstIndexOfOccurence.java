d78376d3ab9507ea73f582998a0410ab4c8f7e06
Rohit <rksanodiya725@gmail.com>
2023-04-24 18:24:43 +0530

    Binary Search 3rd commit

---
+++ src/binarySearch/FirstIndexOfOccurence.java
@@ -0,0 +1,31 @@
+package binarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FirstIndexOfOccurence {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int x=sc.nextInt();
+		int [] arr=Arrays.stream(str.split(",")).mapToInt(Integer:: parseInt).toArray();
+		int n=arr.length;
+		System.out.println(getFirstIndex(arr,n,x));
+		sc.close();
+	}
+	
+	public static int getFirstIndex(int arr[], int n, int x) {
+		int low=0;
+		int high=n-1;
+		while(low<=high) {
+			int mid=(low+high)/2;
+			if(arr[mid]==x && (mid==0 || arr[mid-1]!=arr[mid])) return mid;
+			else if(arr[mid]<x) low=mid+1;
+			else high=mid-1;
+		}
+		
+		return -1;
+	}
+
+}
